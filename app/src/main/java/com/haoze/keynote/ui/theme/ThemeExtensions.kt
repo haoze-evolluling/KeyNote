0432052ce08364119e205403f699092607ef29e5
haoze <leehaoze2022@gmail.com>
2026-05-31 01:11:13 +0800

    feat: 创建向后兼容扩展函数

---
+++ app/src/main/java/com/haoze/keynote/ui/theme/ThemeExtensions.kt
@@ -0,0 +1,8 @@
+package com.haoze.keynote.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+val MaterialTheme.appColors: AppColors
+    @Composable
+    get() = LocalAppColors.current
