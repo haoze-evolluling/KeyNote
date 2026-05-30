cfeea0743aced3063d1e597751482acac9ffd8ce
haoze <leehaoze2022@gmail.com>
2026-05-31 01:06:57 +0800

    feat: 创建 LocalAppColors CompositionLocal

---
+++ app/src/main/java/com/haoze/keynote/ui/theme/LocalAppColors.kt
@@ -0,0 +1,7 @@
+package com.haoze.keynote.ui.theme
+
+import androidx.compose.runtime.staticCompositionLocalOf
+
+val LocalAppColors = staticCompositionLocalOf<AppColors> {
+    error("No AppColors provided")
+}
